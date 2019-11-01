fd16554430fc182d34c8a830e441719e7a40e71a
barkev2009 <barkev2009@bk.ru>
2019-11-01 09:06:03 +0700

    Adding dot cycles

--- Project3.py
+++ Project3.py
@@ -10,16 +10,18 @@ far from completely ready and designed)
 number = int(input('How many dots do you want to enter? '))
 dots = create_dots(number)
 
-print(dots)
+print('\nHere`s the list of dots you`ve created:\n')
+for k, v in dots.items():
+    print('{}:   {}'.format(k, v))
 
 # Creating the dictionary of distances of close-related dots
 delete_indexes = form_deletion_indexes(dots)
 
-print(delete_indexes)
-
 # Deleting the close-related dots
 while delete_indexes:
     deletion_process(delete_indexes, dots)
-    print(delete_indexes)
+    print('')
 
-print(dots)
+print('\nHere`s the corrected list of dots you`ve created (the order numbers are initial):\n')
+for k, v in dots.items():
+    print('{}:   {}'.format(k, v))
