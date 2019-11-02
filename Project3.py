285a735557f1663e990a69d94f0c548b28596259
barkev2009 <barkev2009@bk.ru>
2019-11-02 19:21:12 +0700

    Khan-Buz

--- Project3.py
+++ Project3.py
@@ -1,46 +1,5 @@
 from paths_project3 import *
 
-screen = Screen()
-screen.bgpic('Map.png')
-screen_width = 1400
-screen_height = 920
-sq_size = 10
-screen.screensize(screen_width, screen_height)
-
-
-def grid():
-    grid1 = Turtle()
-    grid1.speed(0)
-    grid1.penup()
-    grid1.goto(-screen_width / 2, -screen_height / 2)
-    grid1.pendown()
-
-    for i in range(int(screen_height/sq_size)):
-        grid1.forward(screen_width)
-        grid1.left(90)
-        grid1.forward(sq_size)
-        grid1.left(90)
-        grid1.forward(screen_width)
-        grid1.right(90)
-        grid1.forward(sq_size)
-        grid1.right(90)
-
-    grid1.penup()
-    grid1.setheading(-90)
-    grid1.goto(-screen_width / 2, screen_height / 2)
-    grid1.pendown()
-
-    for i in range(int(screen_width/sq_size)):
-        grid1.forward(screen_height)
-        grid1.left(90)
-        grid1.forward(sq_size)
-        grid1.left(90)
-        grid1.forward(screen_height)
-        grid1.right(90)
-        grid1.forward(sq_size)
-        grid1.right(90)
-
-
 # grid()
 
 person.penup()
@@ -48,7 +7,6 @@ person.goto(-46*sq_size, 26*sq_size)
 # place = textinput('Программа покажет маршрут до кафе, в которое Вы хотите пойти', 'Какое кафе предпочитаете?')
 
 person.pendown()
-person.goto(control_points[0][0]*sq_size, control_points[0][1]*sq_size)
-person.goto(control_points[1][0]*sq_size, control_points[1][1]*sq_size)
+visit('Khan-Buz')
 
 done()
--- paths_project3.py
+++ paths_project3.py
@@ -1,9 +1,52 @@
 from turtle import *
 
+# Creating screen class
+screen = Screen()
+screen.bgpic('Map.png')
+screen_width = 1400
+screen_height = 920
+sq_size = 10
+screen.screensize(screen_width, screen_height)
+
+
+# Creating function to draw grid
+def grid():
+    grid1 = Turtle()
+    grid1.speed(0)
+    grid1.penup()
+    grid1.goto(-screen_width / 2, -screen_height / 2)
+    grid1.pendown()
+
+    for i in range(int(screen_height / sq_size)):
+        grid1.forward(screen_width)
+        grid1.left(90)
+        grid1.forward(sq_size)
+        grid1.left(90)
+        grid1.forward(screen_width)
+        grid1.right(90)
+        grid1.forward(sq_size)
+        grid1.right(90)
+
+    grid1.penup()
+    grid1.setheading(-90)
+    grid1.goto(-screen_width / 2, screen_height / 2)
+    grid1.pendown()
+
+    for i in range(int(screen_width / sq_size)):
+        grid1.forward(screen_height)
+        grid1.left(90)
+        grid1.forward(sq_size)
+        grid1.left(90)
+        grid1.forward(screen_height)
+        grid1.right(90)
+        grid1.forward(sq_size)
+        grid1.right(90)
+
+
 # Creating person
 person = Turtle()
 person.shape('circle')
-person.shapesize(1)
+person.pensize(3)
 person.color('red')
 person.speed(0)
 
@@ -29,5 +72,17 @@ cafes = ['Kuzina',
          'Cinnabon',
          'Co.mein']
 
-control_points = [[-43, 25],       # Intersection of Pirogova and Univer Prospect
-                  [-60, 4]]        # Turning point in the forest
+control_points = [[-43, 25],  # Intersection of Pirogova and Univer Prospect
+                  [-60, 4],   # Turning point in the forest
+                  [-64, 1],   # Exit off the forest
+                  [-65, -3]]  # Khan-Buz location
+
+paths = {'Khan-Buz': [control_points[0],
+                      control_points[1],
+                      control_points[2],
+                      control_points[3]]}
+
+
+def visit(name):
+    for i in paths[name]:
+        person.goto(sq_size * i[0], sq_size * i[1])
