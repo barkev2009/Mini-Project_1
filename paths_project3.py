d493811b4e62800ec1996d334795c8e5dafc0e9a
barkev2009 <barkev2009@bk.ru>
2019-11-02 18:53:37 +0700

    Restructuring repository

---
+++ paths_project3.py
@@ -0,0 +1,33 @@
+from turtle import *
+
+# Creating person
+person = Turtle()
+person.shape('circle')
+person.shapesize(1)
+person.color('red')
+person.speed(0)
+
+# Creating list of various places to eat or drink at
+cafes = ['Kuzina',
+         'Puerto',
+         'Coffee Collective',
+         'Myasoroob',
+         'Teahuppo',
+         'Coffee Academy',
+         'Spot&Choose'
+         'RybaRis',
+         'Kotocafe',
+         'Clever',
+         'Dudnik',
+         'Fabrika Pizza',
+         'Vilka-Lozhka',
+         'Pechki-Lavochki',
+         'Shurubor',
+         'Pick-Up Coffee',
+         'Khan-Buz',
+         'Uncle Dohner',
+         'Cinnabon',
+         'Co.mein']
+
+control_points = [[-43, 25],       # Intersection of Pirogova and Univer Prospect
+                  [-60, 4]]        # Turning point in the forest
